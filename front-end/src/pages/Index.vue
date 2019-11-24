8b22fea44fa2b4e1dde90542ff4b9c2bc87219fa
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-11-24 13:57:40 -0300

    front-end: Index.vue: fix indentation
    Major indentation fix done with VS Code automatic indentation.

--- front-end/src/pages/Index.vue
+++ front-end/src/pages/Index.vue
@@ -1,182 +1,194 @@
 <template>
-
   <q-page class="flex flex-center">
     <!--<img alt="Quasar logo" src="~assets/quasar-logo-full.svg">-->
 
     <div class="row q-pa-md q-gutter-md">
+      <div class="q-gutter-sm row">
+        <div>
+          <q-field label="Option A - Enter AWS Credentials"></q-field>
 
-       <div class="q-gutter-sm row "><div>
+          <q-card class="my-card">
+            <q-card-section>
+              <q-input v-model="ggName" label="IoT Core Group Name" />
 
-        <q-field label="Option A - Enter AWS Credentials">
-        </q-field>
+              <q-input v-model="keyid" label="Access key ID" />
 
-        <q-card class="my-card">
-          <q-card-section>
+              <q-input style="margin-bottom: 20px" v-model="key" label="Secret access key" />
 
-            <q-input v-model="ggName" label="IoT Core Group Name" />
-
-            <q-input v-model="keyid" label="Access key ID" />
-
-            <q-input style="margin-bottom: 20px" v-model="key" label="Secret access key" />
-
-            <q-btn @click="runBigBang()" type="button" icon="cloud_done" color="primary" label="Run CloudFormation" class="full-width" >
-              <template v-slot:loading>
-                <q-spinner-facebook />
-              </template>
-            </q-btn>
-
-            <q-linear-progress stripe rounded style="height: 26px" class="q-mt-lg" :value="progress" >
-              <div class="absolute-full flex flex-center">
-                <q-badge color="white" text-color="blue" :label="progressLabel" ></q-badge>
-              </div>
-            </q-linear-progress>
-
-            <p v-show="weburls">Your web dashboard is available at:<br />{{weburls}}</p>
-
-          </q-card-section>
-        </q-card>
-
-      </div></div>
-
-      <div class="q-gutter-sm row "><div>
-
-        <q-field label="Option B - Bundle File Upload">
-        </q-field>
-
-        <q-card class="my-card">
-          <q-card-section>
-            <q-uploader
-              style="margin-bottom: 10px"
-              field-name="tar"
-              with-credentials=true
-              auto-upload
-              url="/bundle-tar"
-              label="Upload <hash>.tar.gz  Bundle File"
-            ></q-uploader>
-
-            <form @submit.prevent="updtCredentials" class="q-pa-md">
-              <q-btn type="submit" :loading="updting" icon="update" color="primary" label="Apply credentials" class="full-width" >
+              <q-btn
+                @click="runBigBang()"
+                type="button"
+                icon="cloud_done"
+                color="primary"
+                label="Run CloudFormation"
+                class="full-width"
+              >
                 <template v-slot:loading>
                   <q-spinner-facebook />
                 </template>
               </q-btn>
-            </form>
 
-          </q-card-section>
-        </q-card>
+              <q-linear-progress
+                stripe
+                rounded
+                style="height: 26px"
+                class="q-mt-lg"
+                :value="progress"
+              >
+                <div class="absolute-full flex flex-center">
+                  <q-badge color="white" text-color="blue" :label="progressLabel"></q-badge>
+                </div>
+              </q-linear-progress>
 
-      </div></div>
-      <div class="q-gutter-sm row"><div>
+              <p v-show="weburls">
+                Your web dashboard is available at:
+                <br />
+                {{weburls}}
+              </p>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
 
-        <q-field label="Option C - Single Files Upload">
-        </q-field>
+      <div class="q-gutter-sm row">
+        <div>
+          <q-field label="Option B - Bundle File Upload"></q-field>
 
-         <q-card class="my-card">
-          <q-card-section>
-            
-            <q-uploader
-              style="margin-bottom: 10px"
-              field-name="confjson"
-              with-credentials=true
-              auto-upload
-              url="/conf-json"
-              label="Upload config.json File"
-            ></q-uploader>
+          <q-card class="my-card">
+            <q-card-section>
+              <q-uploader
+                style="margin-bottom: 10px"
+                field-name="tar"
+                with-credentials="true"
+                auto-upload
+                url="/bundle-tar"
+                label="Upload <hash>.tar.gz  Bundle File"
+              ></q-uploader>
 
-            <q-uploader
-              style="margin-bottom: 10px"
-              field-name="pem"
-              with-credentials=true
-              auto-upload
-              url="/cert-pem"
-              label="Upload <hash>.cert.pem File"
-            ></q-uploader>
+              <form @submit.prevent="updtCredentials" class="q-pa-md">
+                <q-btn
+                  type="submit"
+                  :loading="updting"
+                  icon="update"
+                  color="primary"
+                  label="Apply credentials"
+                  class="full-width"
+                >
+                  <template v-slot:loading>
+                    <q-spinner-facebook />
+                  </template>
+                </q-btn>
+              </form>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <div class="q-gutter-sm row">
+        <div>
+          <q-field label="Option C - Single Files Upload"></q-field>
 
-            <q-uploader
-              field-name="privkey"
-              with-credentials=true
-              auto-upload
-              url="/cert-priv-key"
-              label="Upload <hash>.private.key File"
-            ></q-uploader>
+          <q-card class="my-card">
+            <q-card-section>
+              <q-uploader
+                style="margin-bottom: 10px"
+                field-name="confjson"
+                with-credentials="true"
+                auto-upload
+                url="/conf-json"
+                label="Upload config.json File"
+              ></q-uploader>
 
-            <form @submit.prevent="updtCredentials" class="q-pa-md">
-              <q-btn type="submit" :loading="updting" icon="update" color="primary" label="Apply credentials" class="full-width" >
-                <template v-slot:loading>
-                  <q-spinner-facebook />
-                </template>
-              </q-btn>
-            </form>
+              <q-uploader
+                style="margin-bottom: 10px"
+                field-name="pem"
+                with-credentials="true"
+                auto-upload
+                url="/cert-pem"
+                label="Upload <hash>.cert.pem File"
+              ></q-uploader>
 
-          </q-card-section>
-        </q-card>
+              <q-uploader
+                field-name="privkey"
+                with-credentials="true"
+                auto-upload
+                url="/cert-priv-key"
+                label="Upload <hash>.private.key File"
+              ></q-uploader>
 
-      </div></div>
+              <form @submit.prevent="updtCredentials" class="q-pa-md">
+                <q-btn
+                  type="submit"
+                  :loading="updting"
+                  icon="update"
+                  color="primary"
+                  label="Apply credentials"
+                  class="full-width"
+                >
+                  <template v-slot:loading>
+                    <q-spinner-facebook />
+                  </template>
+                </q-btn>
+              </form>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
 
-      <div class="q-gutter-sm row"><div>
+      <div class="q-gutter-sm row">
+        <div>
+          <q-field label="Tool Options"></q-field>
 
-        <q-field label="Tool Options">
-        </q-field>
-
-        <form @submit.prevent="disableForever" class="q-pa-md">
-            <q-btn type="submit" :loading="disabling" icon="tv_off" color="primary" label="Disable this UI forever" class="full-width" >
+          <form @submit.prevent="disableForever" class="q-pa-md">
+            <q-btn
+              type="submit"
+              :loading="disabling"
+              icon="tv_off"
+              color="primary"
+              label="Disable this UI forever"
+              class="full-width"
+            >
               <template v-slot:loading>
                 <q-spinner-facebook />
               </template>
             </q-btn>
+          </form>
+        </div>
+      </div>
 
-        </form>
+      <q-dialog v-model="prompt" persistent>
+        <q-card style="min-width: 350px">
+          <q-card-section>
+            <div class="text-h6">{{loadText}}</div>
+          </q-card-section>
 
-      </div></div>
+          <q-card-section :style="dispSpinner" style="text-align: center;">
+            <q-spinner color="primary" size="3em" :thickness="10" />
+          </q-card-section>
 
-    <q-dialog v-model="prompt" persistent>
-      <q-card style="min-width: 350px">
-        <q-card-section>
-          <div class="text-h6">{{loadText}}</div>
-        </q-card-section>
+          <q-card-section
+            :style="dispError"
+            style="text-align: center; color: red; font-size: 120px;"
+          >
+            <q-icon name="error_outline" />
+          </q-card-section>
 
-        <q-card-section
-          :style="dispSpinner"
-          style="text-align: center;">
-          <q-spinner
-            color="primary"
-            size="3em"
-            :thickness="10"
-          />
-         </q-card-section>
+          <q-card-section
+            :style="dispSuccess"
+            style="text-align: center; color: green; font-size: 120px;"
+          >
+            <q-icon name="insert_emoticon" />
+          </q-card-section>
 
-        <q-card-section
-          :style="dispError"
-          style="text-align: center; color: red; font-size: 120px;"
-        >
-          <q-icon
-            name="error_outline"
-          />
-        </q-card-section>
+          <q-card-section style="text-align: center;">
+            <div class="text-h10">{{descText}}</div>
+          </q-card-section>
 
-        <q-card-section
-          :style="dispSuccess"
-          style="text-align: center; color: green; font-size: 120px;"
-        >
-          <q-icon
-            name="insert_emoticon"
-          />
-        </q-card-section>
-
-         <q-card-section
-          style="text-align: center;"
-         >
-          <div class="text-h10">{{descText}}</div>
-         </q-card-section>
-
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Ok" :disabled="canDisabled" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Ok" :disabled="canDisabled" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </div>
-
   </q-page>
 </template>
 
@@ -192,10 +204,10 @@ import {
   QTooltip,
   QLinearProgress,
   QBadge
-} from 'quasar'
+} from "quasar";
 
 export default {
-  name: 'PageIndex',
+  name: "PageIndex",
   components: {
     QInput,
     QToggle,
@@ -205,7 +217,7 @@ export default {
     QLinearProgress,
     QBadge
   },
-  data () {
+  data() {
     return {
       loadText: "Loading",
       descText: "Cloud Formation Success",
@@ -217,150 +229,153 @@ export default {
       updting: false,
       prompt: false,
       canDisabled: true,
-      address: '',
-      keyid: '',
-      key: '',
+      address: "",
+      keyid: "",
+      key: "",
       progress: 0.0,
-      weburls: ''
-    }
+      weburls: ""
+    };
   },
   computed: {
-    progressLabel () {
-      return (this.progress * 100).toFixed(2) + '%'
+    progressLabel() {
+      return (this.progress * 100).toFixed(2) + "%";
     }
   },
   methods: {
-    updtProgressBar () {
+    updtProgressBar() {
       var me = this;
       setInterval(() => {
-        this.$axios.post('/progress', {
-          updatecredentials: true
-        })
-        .then(function (res) {
-          if (res.status == 200) {
-            me.progress = Number(res.data)/100;
-          }
-        })
-        .catch(function (err) {
-          console.log("Error on POST to /progress" + err);
-        });
+        this.$axios
+          .post("/progress", {
+            updatecredentials: true
+          })
+          .then(function(res) {
+            if (res.status == 200) {
+              me.progress = Number(res.data) / 100;
+            }
+          })
+          .catch(function(err) {
+            console.log("Error on POST to /progress" + err);
+          });
       }, 5000);
     },
-    getWebURL () {
+    getWebURL() {
       var me = this;
       setInterval(() => {
-        this.$axios.post('/webdashboards', {
-          updatecredentials: true
-        })
-        .then(function (res) {
-          if (res.status == 200) {
-            me.weburls = res.data;
-          }
-        })
-        .catch(function (err) {
-          console.log("Error on POST to /webdashboards" + err);
-        });
+        this.$axios
+          .post("/webdashboards", {
+            updatecredentials: true
+          })
+          .then(function(res) {
+            if (res.status == 200) {
+              me.weburls = res.data;
+            }
+          })
+          .catch(function(err) {
+            console.log("Error on POST to /webdashboards" + err);
+          });
       }, 30000);
     },
-    disableForever () {
-      this.disabling = true
+    disableForever() {
+      this.disabling = true;
 
-      this.$axios.post('/disable', {
-        withCredentials: true,
-        disable: true
-      })
-      .then(function (res) {
-        console.log(res);
-      })
-      .catch(function (err) {
-        console.log("Error on POST to /disable" + err);
-      });
-
-      setTimeout(() => {
-        this.disabling = false
-      }, 3000)
-    },
-    updtCredentials () {
-      this.updting = true
-
-      this.$axios.post('/updatecredentials', {
-        updatecredentials: true
-      })
-      .then(function (res) {
-        console.log(res);
-      })
-      .catch(function (err) {
-        console.log("Error on POST to /updatecredentials" + err);
-      });
+      this.$axios
+        .post("/disable", {
+          withCredentials: true,
+          disable: true
+        })
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log("Error on POST to /disable" + err);
+        });
 
       setTimeout(() => {
-        this.updting = false
-      }, 3000)
+        this.disabling = false;
+      }, 3000);
     },
-    runBigBang () {
+    updtCredentials() {
+      this.updting = true;
 
+      this.$axios
+        .post("/updatecredentials", {
+          updatecredentials: true
+        })
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log("Error on POST to /updatecredentials" + err);
+        });
+
+      setTimeout(() => {
+        this.updting = false;
+      }, 3000);
+    },
+    runBigBang() {
       this.prompt = true;
       var me = this;
-      me.canDisabled = true
-      me.dispSuccess = "display: none;"
-      me.dispError = "display: none;"
-      me.dispSpinner = "display: block;"
-      me.descText = ""
-      me.loadText = "Running ..."
+      me.canDisabled = true;
+      me.dispSuccess = "display: none;";
+      me.dispError = "display: none;";
+      me.dispSpinner = "display: block;";
+      me.descText = "";
+      me.loadText = "Running ...";
 
       console.info(me.key);
       console.info(me.keyid);
 
       if (me.keyid != "" && me.key != "") {
-        this.$axios.post('/bigbang', 
-        {
-          keyId: me.keyid,
-          key: me.key,
-          ggName: me.ggName
-        })
-        .then( (res) => {
-          console.log("big bang call");
-          console.info(res);
+        this.$axios
+          .post("/bigbang", {
+            keyId: me.keyid,
+            key: me.key,
+            ggName: me.ggName
+          })
+          .then(res => {
+            console.log("big bang call");
+            console.info(res);
 
-          if (res.status == 200) {
-            me.canDisabled = false
-            me.dispSuccess = "display: block;"
-            me.dispError = "display: none;"
-            me.dispSpinner = "display: none;"
-            me.descText = ""
-            me.loadText = "Deployment is running. It takes time, around 1 hour."
-          } else {
-            me.canDisabled = false
-            me.dispSuccess = "display: none;"
-            me.dispError = "display: block;"
-            me.dispSpinner = "display: none;"
-            me.descText = "Error trying to run AWS CloudFormation"
-            me.loadText = "Error"
-          }
-        })
-        .catch(function (err) {
-          console.log("Error on POST to /bigbang " + err);
-          me.dispSpinner = "display: none;"
-          me.dispSuccess = "display: none;"
-          me.loadText = "Error"
-          me.descText = "Error trying to connect to endpoint"
-          me.dispError = "display: block;"
-          me.canDisabled = false
-        });
+            if (res.status == 200) {
+              me.canDisabled = false;
+              me.dispSuccess = "display: block;";
+              me.dispError = "display: none;";
+              me.dispSpinner = "display: none;";
+              me.descText = "";
+              me.loadText =
+                "Deployment is running. It takes time, around 1 hour.";
+            } else {
+              me.canDisabled = false;
+              me.dispSuccess = "display: none;";
+              me.dispError = "display: block;";
+              me.dispSpinner = "display: none;";
+              me.descText = "Error trying to run AWS CloudFormation";
+              me.loadText = "Error";
+            }
+          })
+          .catch(function(err) {
+            console.log("Error on POST to /bigbang " + err);
+            me.dispSpinner = "display: none;";
+            me.dispSuccess = "display: none;";
+            me.loadText = "Error";
+            me.descText = "Error trying to connect to endpoint";
+            me.dispError = "display: block;";
+            me.canDisabled = false;
+          });
       } else {
-        me.dispSpinner = "display: none;"
-        me.dispSuccess = "display: none;"
-        me.loadText = "Error"
-        me.descText = "Empty Fields"
-        me.dispError = "display: block;"
-        me.canDisabled = false
+        me.dispSpinner = "display: none;";
+        me.dispSuccess = "display: none;";
+        me.loadText = "Error";
+        me.descText = "Empty Fields";
+        me.dispError = "display: block;";
+        me.canDisabled = false;
       }
     }
   },
-  created () {
+  created() {
     this.updtProgressBar();
     this.getWebURL();
   }
-
-}
+};
 </script>
